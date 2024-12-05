34adf9f8a709f011e48203aab512eef529f6c1b4
Adebola <bola@neegles.com>
2024-12-06 00:32:44 +0100

    install and setup supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/supabase",
+  ],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  colorMode: {
+    classSuffix: "",
+  },
+  supabase: {
+    redirectOptions: {
+      login: "/auth",
+      callback: "/confirm",
+      exclude: ["/"],
+    },
+  },
+});
---
+++ pages/auth.vue
@@ -0,0 +1,21 @@
+<!-- App.vue -->
+<template>
+  <Auth
+    :supabaseClient="supabaseClient"
+    :appearance="{
+      theme: ThemeSupa,
+    }"
+  />
+</template>
+
+<script setup lang="ts">
+// Import predefined theme
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { Auth } from "@supa-kit/auth-ui-vue";
+
+const supabaseClient = useSupabaseClient();
+
+definePageMeta({
+  layout: "home",
+});
+</script>
